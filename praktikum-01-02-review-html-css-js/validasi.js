880c60d6a6d1f14f62ec3e67edf8626fdd87c26a
Vanky <vankystar7@gmail.com>
2022-10-18 15:52:15 +0800

    commit tugas praktikum-01-02

---
+++ praktikum-01-02-review-html-css-js/validasi.js
@@ -0,0 +1,8 @@
+function validasi() {
+    let txtKataKunci = document.getElementById("txtKataKunci");
+    if (txtKataKunci.value == "") {
+        alert("Anda harus mengisi textbox dengan sembarang kata");
+    } else {
+        alert("Anda ingin mencari dengan kata kunci = " + txtKataKunci.value);
+    }
+}
